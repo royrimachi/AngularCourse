21eb751ab04477fa51fef7f373a746bf61f47109
royexr <royexrc@gmail.com>
2020-07-21 23:59:27 -0500

    Set routes to recipe list and use active router in recipe item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
 import { DropdownDirective } from './shared/dropdown.directive';
 import { DropdownMenuDirective } from './shared/dropdown-menu.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
     ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDirective,
-    DropdownMenuDirective
+    DropdownMenuDirective,
+    RecipeStartComponent
   ],
   imports: [
     AppRoutingModule,
