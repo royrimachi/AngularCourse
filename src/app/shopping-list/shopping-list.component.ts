af4b73b1c6afef1b6330ddbcffaab333056d03f0
royexr <royexrc@gmail.com>
2020-07-23 14:08:12 -0500

    CRUD for shopping edit finished

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -25,6 +25,10 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
     )
   }
 
+  onEditItem(index: number) {
+    this.slService.starterdEditing.next(index);
+  }
+
   ngOnDestroy() {
     this.iCSubscription.unsubscribe();
   }
