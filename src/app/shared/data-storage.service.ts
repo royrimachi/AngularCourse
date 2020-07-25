75e36f1d90b9046ed164b77f6d42be9c81533d5a
royexr <royexrc@gmail.com>
2020-07-24 22:10:33 -0500

    Fetching data

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
 
 @Injectable({
   providedIn: 'root'
@@ -23,5 +24,15 @@ export class DataStorageService {
       .subscribe(response => {
         console.log(response);
       });
+  }
+
+  fetchRecipes() {
+    this.http
+      .get<Recipe[]>(
+        'https://recipe-book-6449d.firebaseio.com/recipes.json',
+      )
+      .subscribe(recipes => {
+        this.recipeService.setRecipes(recipes);
+      })
   }
 }
